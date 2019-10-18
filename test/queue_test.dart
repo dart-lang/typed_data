9d7701e10f9c630e5a1ca9c8a0422eddd4ff3bf8
Natalie Weizenbaum <nweiz@google.com>
2019-10-18 10:23:30 +0100

    Ignore an unhelpful lint

--- test/queue_test.dart
+++ test/queue_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// ignore_for_file: avoid_function_literals_in_foreach_calls
+
 import "package:test/test.dart";
 
 import "package:typed_data/typed_data.dart";
